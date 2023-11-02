50c06be55a3d0cbfbe88675763730be36ed02f08
will <will@3165d.com>
2023-11-02 09:14:50 -0400

    Add stuff
    Fix a thing

--- Entities/Submarine.cs
+++ Entities/Submarine.cs
@@ -1,16 +1,19 @@
-﻿namespace Entities
+﻿using Mechanics.Composition;
+using Mechanics;
+
+namespace Entities
 {
-    public class Submarine
+    public class Submarine : ILocatable
     {
         //boomers, fast attack subs, frigates, sea mines, etc 
-        private Mechanics.Coordinate _position;
-        public Mechanics.Coordinate Position => _position;
+        private Mechanics.Coordinate _location;
+        public Mechanics.Coordinate Location => _location;
         public Submarine()
         {
                 
         }
 
-        public void Move()
+        public void Move(Coordinate target)
         {
 
         }
